f6dbe5cf6039833b44107a70e715ec943a2f48ad
hasan <hasanakoz1986@gmail.com>
2022-09-28 19:55:48 +0300

    main js dom codes created

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let isError = false;
-cityName = "London";
+let city = document.querySelector("#city");
+const btn = document.querySelector("#submit");
+
 const getWeather = async function () {
-  const API_KEY = `b8bd59d8cfb70eee06f98d68a4554461`;
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid = ${API_KEY}`;
+  const API_KEY = "b8bd59d8cfb70eee06f98d68a4554461";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
 
   try {
     const res = await fetch(url);
@@ -17,5 +19,36 @@ const getWeather = async function () {
   }
 };
 
-const renderWeather = () => {};
-getWeather();
+const renderWeather = (data) => {
+  const weatherCard = document.querySelector(".weather-cart");
+
+  if (isError) {
+    weatherCard.innerText = `weather conditions  cannot be fetched`;
+  }
+
+  const {
+    name,
+    main: { feels_like, temp, temp_max, temp_min, humidity },
+    weather: { main, description, icon },
+    wind: { speed },
+  } = data;
+
+  weatherCard.innerHTML += `<div class="card" style="width: 18rem;">
+    <div class="card-header">
+      ${city.toUpperCase()}  
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Temperature: ${temp}  Feels Like ${feels_like}</li>
+      <li class="list-group-item"> Wind: ${speed}   </li>
+      <li class="list-group-item"> ${data.weather[0].icon}  ${
+    data.weather[0].description
+  }   </li>
+      <li class="list-group-item">Humidity: ${humidity}</li>
+    </ul>
+    </div>"`;
+  console.log(city.value);
+};
+btn.addEventListener("click", () => {
+  city = city.value;
+  getWeather();
+});
