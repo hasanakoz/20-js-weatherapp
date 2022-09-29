4edfb74dacbdba0c78830284ea57788105ef9b03
hasan <hasanakoz1986@gmail.com>
2022-09-29 15:02:45 +0300

    fetch bugs fixed

--- app.js
+++ app.js
@@ -1,54 +1,54 @@
-let isError = false;
+// let isError = false;
 let city = document.querySelector("#city");
 const btn = document.querySelector("#submit");
+const form = document.querySelector("form");
+const weatherCard = document.querySelector(".weather-cart");
 
-const getWeather = async function () {
-  const API_KEY = "b8bd59d8cfb70eee06f98d68a4554461";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-
-  try {
-    const res = await fetch(url);
-    if (!res.ok) {
-      isError = true;
-    }
-    const data = await res.json();
-    renderWeather(data);
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const renderWeather = (data) => {
-  const weatherCard = document.querySelector(".weather-cart");
-
-  if (isError) {
-    weatherCard.innerText = `weather conditions  cannot be fetched`;
-  }
-
-  const {
-    name,
-    main: { feels_like, temp, temp_max, temp_min, humidity },
-    weather: { main, description, icon },
-    wind: { speed },
-  } = data;
-
-  weatherCard.innerHTML += `<div class="card" style="width: 18rem;">
-    <div class="card-header">
-      ${city.toUpperCase()}  
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">Temperature: ${temp}  Feels Like ${feels_like}</li>
-      <li class="list-group-item"> Wind: ${speed}   </li>
-      <li class="list-group-item"> ${data.weather[0].icon}  ${
-    data.weather[0].description
-  }   </li>
-      <li class="list-group-item">Humidity: ${humidity}</li>
-    </ul>
-    </div>"`;
-  console.log(city.value);
-};
-btn.addEventListener("click", () => {
-  city = city.value;
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
   getWeather();
 });
+
+const getWeather = async function () {
+  const units = "metric";
+  const cityValue = city.value;
+
+  const API_KEY = "b8bd59d8cfb70eee06f98d68a4554461";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&units=${units}&appid=${API_KEY}`;
+  try {
+    const res = await fetch(url);
+    // if (!res.ok) {
+    //   throw new Error();
+    //   // isError = true;
+    // }
+    const data = await res.json();
+    console.log(data);
+
+    const {
+      name,
+      main: { feels_like, temp, temp_max, temp_min, humidity },
+      weather: { main, description, icon },
+      wind: { speed },
+    } = data;
+    const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+    let list = document.createElement("li");
+
+    list.innerHTML += `<div class="card" style="width: 18rem;">
+      <div class="card-header">
+        ${cityValue.toUpperCase()}  
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Temperature: ${temp}  Feels Like ${feels_like}</li>
+        <li class="list-group-item"> Wind: ${speed}   </li>
+        <li class="list-group-item"> <img src="${iconUrl}"</li>
+        <li class="list-group-item">Humidity: ${humidity}</li>
+        <li class="list-group-item"> ${data.weather[0].description}   </li>
+      </ul>
+      </div>`;
+    document.querySelector(".weather-cart ul").prepend(list);
+    form.reset();
+  } catch (error) {
+    weatherCard.innerText = `weather conditions  cannot be fetched`;
+  }
+};
